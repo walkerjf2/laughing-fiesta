4764e7b7ae1139eb9cb94c5e8cfbcb6427d38913
walkerjf2 <59940698+walkerjf2@users.noreply.github.com>
2020-01-15 18:22:21 -0500

    Add files via upload
    Hello. java

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/*
+Hello.java
+
+Julia Walker
+January 15, 2020
+CMSC 255 
+*/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Julia Walker>");
+		System.out.println("<Hello.java.>");
+		System.out.println("<It teaches you about java.>");
+		System.out.println("<Version 11>");
+		System.out.println("<CMSC 255 and Section 901>");
+		System.out.println();
+		}
+}
+
+
+
+
+
